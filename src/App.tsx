d0c517296af00ef04e23324562191533f60e5701
katakanakun <fomachanyade@gmail.com>
2024-01-24 12:18:30 +0900

    feat: add MultipleContainers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { MultipleContainers } from "./dnd/multiple_containers";
 import { Simple } from "./dnd/simple";
 import { Sortable } from "./dnd/sortable";
 
@@ -14,6 +15,10 @@ function App() {
 				<h2>sortable</h2>
 				<Sortable />
 			</div>
+			<div style={{ margin: "16 0" }}>
+				<h2>multiple containers</h2>
+				<MultipleContainers />
+				</div>
 		</div>
 	);
 }
---
+++ src/dnd/multiple_containers/index.tsx
@@ -0,0 +1,3 @@
+export const MultipleContainers = () => {
+    return <div>MultipleContainers</div>;
+};
