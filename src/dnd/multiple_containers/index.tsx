240dc42be9f0ed3ef94a8cacbf48a4683cb8dfc2
katakanakun <fomachanyade@gmail.com>
2024-01-24 14:29:54 +0900

    feat: drag overlayを追加

--- src/dnd/multiple_containers/index.tsx
+++ src/dnd/multiple_containers/index.tsx
@@ -1,16 +1,38 @@
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, DragOverlay, UniqueIdentifier } from "@dnd-kit/core";
 import {
-	SortableContext,
-	horizontalListSortingStrategy,
+    SortableContext,
+    horizontalListSortingStrategy,
+    verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { DroppableContainer } from "../sortable/droppable_container";
+import { useState } from "react";
+import { createRange } from "../lib/createRange";
+import { Item } from "../lib/item";
+import { SortableItem } from "../lib/sortable_item";
+import { DroppableContainer } from "./droppable_container";
+
+type Items = Record<UniqueIdentifier, UniqueIdentifier[]>;
 
 export const MultipleContainers = () => {
-	const containers = [1, 2, 3, 4];
-	const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const itemCount = 5;
+	const [items, setItems] = useState<Items>({
+		A: createRange(itemCount, (index) => `A${index + 1}`),
+		B: createRange(itemCount, (index) => `B${index + 1}`),
+		C: createRange(itemCount, (index) => `C${index + 1}`),
+		D: createRange(itemCount, (index) => `D${index + 1}`),
+	});
+	const [containers, setContainers] = useState(
+		Object.keys(items) as UniqueIdentifier[],
+	);
+    const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
 
 	return (
-		<DndContext>
+		<DndContext
+        onDragStart={({active}) => {
+            setActiveId(active.id);
+          }}
+          onDragEnd={({active})=>{
+            setActiveId(null);
+          }}>
 			<div
 				style={{
 					display: "inline-grid",
@@ -23,11 +45,11 @@ export const MultipleContainers = () => {
 					items={[...containers]}
 					strategy={horizontalListSortingStrategy}
 				>
-					{containers.map((id) => (
+					{containers.map((containerId) => (
 						<DroppableContainer
-							id={id}
-							items={items}
-							key={id}
+							id={containerId}
+							items={items[containerId]}
+							key={containerId}
 							style={{
 								display: "inline-grid",
 								boxSizing: "border-box",
@@ -35,11 +57,20 @@ export const MultipleContainers = () => {
 								gridAutoFlow: "row",
 							}}
 						>
-							droppable
+							<SortableContext
+								items={items[containerId]}
+								strategy={verticalListSortingStrategy}
+							>
+								{items[containerId].map((value, index) => {
+									return <SortableItem id={value as number} />;
+								})}
+							</SortableContext>
 						</DroppableContainer>
 					))}
 				</SortableContext>
 			</div>
+            <DragOverlay>{activeId ? <Item id={activeId as number} /> : null}</DragOverlay>
+
 		</DndContext>
 	);
 };
